ab7ae8c5c889987f25db94a0c28159dbd6dd13f6
mozibrishu <mozib.diu@gmail.com>
2021-03-12 21:48:58 +0600

    Complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import { useState } from 'react';
 
 function App() {
+  const [likeColor, setLikeColor] = useState('');
+  const handleLike = () => {
+    setLikeColor(likeColor ? '' : 'primary');
+  }
+
   return (
     <div className="App">
       <div className="like-btn">
-      <ThumbUpIcon className="thumb-up" style={{fontSize:'180px'}}></ThumbUpIcon>
+        <ThumbUpIcon className="thumb-up" style={{fontSize:'200px'}} onClick={handleLike} color={likeColor}></ThumbUpIcon>
+        <h1>Click to Toggle</h1>
       </div>
     </div>
   );
